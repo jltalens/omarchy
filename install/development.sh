806b43f0cfb256bc5efc03fd1994c6801c1af1e9
Jose Talens <jltalens@outlook.com>
2025-07-14 18:00:13 +0100

    Added nvm

--- install/development.sh
+++ install/development.sh
@@ -3,3 +3,10 @@ yay -S --noconfirm --needed \
   imagemagick \
   mariadb-libs postgresql-libs \
   lazygit lazydocker-bin
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+
+NVM='export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"; [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm'
+echo $NVM >> ~/.zshrc
+echo $NVM >> ~/.bashrc
